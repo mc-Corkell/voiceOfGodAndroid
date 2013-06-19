81388b914d881fbf41088c267f78d350315237cc
mc_corkell <katiemccorkell@gmail.com>
2013-06-18 19:32:54 -0700

    adding things I changed a while ago?

--- VoiceOfGod/src/com/example/voiceofgod/LoginActivity.java
+++ VoiceOfGod/src/com/example/voiceofgod/LoginActivity.java
@@ -30,15 +30,6 @@ import android.support.v4.app.NavUtils;
  */
 public class LoginActivity extends Activity {
     /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello",
-            "bar@example.com:world"
-    };
-
-    /**
      * The default email to populate the email field with.
      */
     public static final String EXTRA_EMAIL = "com.example.android.authenticatordemo.extra.EMAIL";
